e4b15e4eaccac1e362a03a67ec4c2410527ec54b
brandon-pickup <brandonpickup@icloud.com>
2018-08-29 18:43:19 +0200

    Added POJOs for author and message that will be needed later

---
+++ src/main/java/org/interview/twitter/stream/TwitterAuthor.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.interview.twitter.stream;
+
+/**
+ * Class that describes the author of a tweet on Twitter
+ * @author brandonpickup
+ */
+public class TwitterAuthor
+{
+    //instance variable for the author of a tweet
+    private String userID;
+    private String userCreationDate;
+    private String userScreenName;
+    
+    /**
+     * No argument constructor
+     */
+    public TwitterAuthor()
+    {
+        // no default values assigned to instance variables
+    }
+    
+    /**
+     * Constructor
+     * @param userID - String
+     * @param userCreationDate - String
+     * @param userScreenName - String
+     */
+    public TwitterAuthor(String userID, String userCreationDate, String userScreenName)
+    {
+        this.userID = userID;
+        this.userCreationDate = userCreationDate;
+        this.userScreenName = userScreenName;
+    }
+    
+    /**
+     * Sets the userID field for an Author
+     * @param userID - String
+     */
+    public void setUserID(String userID)
+    {
+        this.userID = userID;
+    }
+    
+    /**
+     * Sets the userCreationDate for an Author
+     * @param userCreationDate - String
+     */
+    public void setUserCreationDate(String userCreationDate)
+    {
+        this.userCreationDate = userCreationDate;
+    }
+    
+    /**
+     * Sets the userScreenName for an Author
+     * @param userScreenName - String
+     */
+    public void setUserScreenName(String userScreenName)
+    {
+        this.userScreenName = userScreenName;
+    }
+    
+    /**
+     * Returns the userID for an Author
+     * @return userID 
+     */
+    public String getUserID()
+    {
+        return this.userID;
+    }
+    
+    /**
+     * Returns the userCReationDate for an Author
+     * @return userCReationDate
+     */
+    public String getUserCreationDate()
+    {
+        return this.userCreationDate;
+    }
+    
+    /**
+     * Returns the userScreenName for an Author
+     * @return userScreenName
+     */
+    public String getUserScreenName()
+    {
+        return this.userScreenName;
+    }
+}
---
+++ src/main/java/org/interview/twitter/stream/TwitterMessage.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.interview.twitter.stream;
+
+/**
+ * Class that describes a particular tweet message on Twitter
+ * @author brandonpickup
+ */
+public class TwitterMessage
+{
+    //instance varibale for a tweet message
+    private String messageID;
+    private String messageCreationDate;
+    private String messageText;
+    private TwitterAuthor messageAuthor;
+    
+    /**
+     * No argument constructor
+     */
+    public TwitterMessage()
+    {
+        // no default values assigned to instance variables
+    }
+    
+    /**
+     * Constructor
+     * @param messageID - String
+     * @param messageCreationDate - String
+     * @param messageText - String
+     * @param messageAuthor - TwitterAuthor
+     */
+    public TwitterMessage(String messageID, String messageCreationDate, String messageText, TwitterAuthor messageAuthor)
+    {
+        this.messageID = messageID;
+        this.messageCreationDate = messageCreationDate;
+        this.messageText = messageText;
+        this.messageAuthor = messageAuthor;
+    }
+
+    /**
+     * Returns the messageId of a tweet
+     * @return messageID
+     */
+    public String getMessageID()
+    {
+        return messageID;
+    }
+
+    /**
+     * Sets the messageID of a tweet
+     * @param messageID - String
+     */
+    public void setMessageID(String messageID)
+    {
+        this.messageID = messageID;
+    }
+
+    /**
+     * Returns the messageCreationDate of a tweet
+     * @return messageCreationDate
+     */
+    public String getMessageCreationDate()
+    {
+        return messageCreationDate;
+    }
+
+    /**
+     * Sets the messageCreationDate of a tweet
+     * @param messageCreationDate  - String
+     */
+    public void setMessageCreationDate(String messageCreationDate)
+    {
+        this.messageCreationDate = messageCreationDate;
+    }
+
+    /**
+     * Returns the messageText of a tweet
+     * @return messageText
+     */
+    public String getMessageText()
+    {
+        return messageText;
+    }
+
+    /**
+     * Sets the messageText of a tweet object
+     * @param messageText - String
+     */
+    public void setMessageText(String messageText)
+    {
+        this.messageText = messageText;
+    }
+
+    /**
+     * Returns the messageAuthor of a tweet
+     * @return messageAuthor
+     */
+    public TwitterAuthor getMessageAuthor()
+    {
+        return messageAuthor;
+    }
+
+    /**
+     * Sets the messageAuthor of a tweet
+     * @param messageAuthor - TwitterAuthor
+     */
+    public void setMessageAuthor(TwitterAuthor messageAuthor)
+    {
+        this.messageAuthor = messageAuthor;
+    }
+    
+    
+}
