a23fd534b18b9f824095f8b2ba0c0065a43cc04f
brandon-pickup <brandonpickup@icloud.com>
2018-09-01 14:50:26 +0200

    Added additional logging, as well as tidying data written to file so as to be able to succesfully parse data

--- src/main/java/org/interview/twitter/stream/Main.java
+++ src/main/java/org/interview/twitter/stream/Main.java
@@ -9,29 +9,20 @@ import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpResponse;
-import com.google.api.client.json.GenericJson;
 import com.google.gson.Gson;
-import java.io.BufferedReader;
 import java.io.File;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import org.interview.oauth.twitter.TwitterAuthenticationException;
 import org.interview.oauth.twitter.TwitterAuthenticator;
 
@@ -41,42 +32,46 @@ import org.interview.oauth.twitter.TwitterAuthenticator;
  */
 public class Main
 {    
-    private static final int MAX_NUMBER_OF_MESSAGES =100;
+    private static final int MAX_NUMBER_OF_MESSAGES = 100;
     private static final String CONSUMER_KEY = "vp8qXAMoZzy6jowJdtouPLUUb";
     private static final String CONSUMER_SECRET = "IMx3eIRfXXbRimoIz7cNpZCl0dr9dYEdRuDVTr2C4LdResXjN7";
+    private static long startTime, endTime;
         
     public static void main(String[] args)
     {
-        
+        //File used to write results to and read from later
         File file = new File("output.txt");
         
         //Connect to the API and save output to file
-        //streamTweetsAndSave(file);
+        streamTweetsAndSave(file);
         
         //Obtain a list of TwitterMessages by reading from saved file
         List<TwitterMessage> messages = processFileContentsForTwitterMessages(file);
         
         //sort by user creation date, and for users that have posted more than once, sort by message creation date
+        System.out.println("\nSorting messages");
         Collections.sort(messages);
         
         //print output to console
+        System.out.println("\nMessages sorted and available:");
         printMessagesToConsole(messages);
     }
     
     /**
      * Method to connect to the Stream API of twitter, stream bieber track tweets for 30s and save output to a file
      * The implementation will always stream for 30s before timing out.
-     * Less than 100 entries is handled upon processing the saved file
+     * If there are more than 100 entries at 30s, only the first 100 will be processed (at a later stage)
      * @param saveFile 
      */
     private static void streamTweetsAndSave(File saveFile)
     {
+        
         TwitterAuthenticator authenticator = new TwitterAuthenticator(System.out, CONSUMER_KEY, CONSUMER_SECRET);
         
         //required types that need to be closed
         OutputStream output = null;
-        HttpResponse response = null;
-        
+        HttpResponse httpResponse = null;
+        HttpRequest httpRequest = null;
         try
         {
             HttpRequestFactory httpRequestFactory = authenticator.getAuthorizedHttpRequestFactory();
@@ -84,22 +79,24 @@ public class Main
             String trackValue = "bieber";
             GenericUrl url = new GenericUrl("https://stream.twitter.com/1.1/statuses/filter.json?track="+trackValue);
 
-            HttpRequest httpRequest = httpRequestFactory.buildGetRequest(url);
-            httpRequest.setReadTimeout(3 * 1000);
+            httpRequest = httpRequestFactory.buildGetRequest(url);
+            //only read for 30s
+            httpRequest.setReadTimeout(30 * 1000); 
             
             output = new FileOutputStream(saveFile);
             
-            response = httpRequest.execute();
-            response.download(output);
+            System.out.println("\nStarting to stream");
+            httpResponse = httpRequest.execute();
+            
+            httpResponse.download(output);
         }
         catch (TwitterAuthenticationException ex)
         {
-            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.toString());
         }
         catch (java.net.SocketTimeoutException ex)
         {
-            //Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("30s timeout reached");
+            System.out.println("\n30s timeout reached");
         }
         catch (IOException ex)
         {
@@ -110,7 +107,9 @@ public class Main
             try
             {
                 output.close();
-                response.disconnect();
+                System.out.println("\nOutput file closed");
+                httpResponse.disconnect();
+                System.out.println("\nDisconnected succesfully after timeout");
             } catch (IOException ex)
             {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
@@ -148,16 +147,23 @@ public class Main
         try
         {
             Scanner sc = new Scanner (new FileReader(file));
-            int counter = 0;
             while (sc.hasNext())
             {
                 //do not process more than 100 messages as this is the specification
-                if (++counter > MAX_NUMBER_OF_MESSAGES)
+                if (messages.size() >= MAX_NUMBER_OF_MESSAGES)
                 {
+                    System.out.println("\n100 message limit reached");
                     break;
                 }
-                String line = sc.nextLine();
+                //tidy line and check that it starts with the correct character - {. If not, do not count 
+                String line = sc.nextLine().trim(); 
+                
+                if (!line.startsWith("{"))
+                {
+                    continue;
+                }
                 TwitterMessage message = g.fromJson(line, TwitterMessage.class);
+                
                 messages.add(message);
             }
         } catch (FileNotFoundException ex)
